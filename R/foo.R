e3a6aafbccc8b88882a81983f010447b70cc7c86
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-12-05 10:28:49 +0000

    initial commit

---
+++ R/foo.R
@@ -0,0 +1,13 @@
+#' Placeholder function
+#'
+#' @param x argument
+#'
+#' @return Prints to console, called for side-effects.
+#' @export
+#'
+#' @examples
+#' foo(5)
+#' foo("a")
+foo <- function(x) {
+  print(x)
+}
